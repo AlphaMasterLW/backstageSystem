9f0e516322b4e19e11da815e6bedb0b82f725a7f
Gossip Girl <1045001434@qq.com>
2020-02-28 22:53:59 +0800

    mongoose数据模型搭建

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('hello world')
+/**
+ * $route GET api/users/test
+ * @desc 返回请求的json数据
+ * @access public
+ */
+router.get('/test', function (req, res) {
+    res.json({
+        "msg": "你好啊"
+    })
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+// 引入user.js
+const users  =require('./routes/api/users')
+
 // DB config
 const db = require('./config/keys').mongoURI;
 // connect to mongodb
@@ -15,11 +18,14 @@ mongoose.connect(db,{
 .catch((err) => {
     console.log(err)
 });
+
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World a2!'));
 
+// 使用routes
+app.use('/api/users', users);
 app.listen(port, hostname, () => {
     console.log(`服务器运行在 http://${hostname}:${port}/`);
 });
