4765af5aa224d35c4c7cecb546f80416286424ec
Gossip Girl <1045001434@qq.com>
2020-02-29 15:20:55 +0800

    搭建注册接口并存储信息到数据库

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+
 /**
  * $route GET api/users/test
  * @desc 返回请求的json数据
@@ -11,6 +14,38 @@ const router = express.Router();
 router.get('/test', function (req, res) {
     res.json({
         "msg": "你好啊"
+    })
+});
+
+/**
+ * $route POST api/users/register
+ * @desc 返回请求的json数据
+ * @access public
+ */
+router.post('/register', function (req, res) {
+    // 查询数据库中是否存在邮箱
+    User.findOne({
+        email: req.body.email
+    }).then((user) => {
+        if(user){
+            return res.status(400).json({email: "邮箱已被注册"})
+        }else{
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    // Store hash in your password DB.
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(error => console.log(err))
+                });
+            });
+        }
     })
 });
 
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
 // 引入user.js
-const users  =require('./routes/api/users')
+const users = require('./routes/api/users');
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -22,10 +23,16 @@ mongoose.connect(db,{
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World a2!'));
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 // 使用routes
 app.use('/api/users', users);
+
+app.get('/', (req, res) => res.send('Hello World a2!'));
+
 app.listen(port, hostname, () => {
     console.log(`服务器运行在 http://${hostname}:${port}/`);
 });
